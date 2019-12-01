a4210399c6a88ceb1624aa51d24b713c0d1eba4e
Manuel Bugallo <marvinody@gmail.com>
2019-12-01 14:10:37 -0500

    feat: finishd d02p1

--- 01/code.js
+++ 01/code.js
@@ -1,5 +1,9 @@
 const findFuelForMass = mass => {
-  return Math.floor(mass / 3) - 2
+  if (mass < 0) return 0
+  const fuelNeededForMass = Math.floor(mass / 3) - 2
+  const fuelNeededForFuel = Math.max(0, findFuelForMass(fuelNeededForMass))
+  return fuelNeededForMass + fuelNeededForFuel
+
 }
 
 module.exports = {
