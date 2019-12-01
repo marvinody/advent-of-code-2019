fc71269a672019ee1b350bb6d1b7f65756643058
Manuel Bugallo <marvinody@gmail.com>
2019-12-01 14:55:44 -0500

    feat: finished d01 in go

--- 01/go/main.go
+++ 01/go/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -12,5 +13,27 @@ func main() {
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
-	fmt.Println(lines)
+	driver(lines)
+
+}
+
+// the body of this function will change all the time!
+func driver(lines []string) {
+
+	totalFuel := 0
+	for _, str := range lines {
+		mass, _ := strconv.Atoi(str)
+		fuel := findFuelForMass(mass)
+		totalFuel += fuel
+	}
+	fmt.Println("TotalFuel:", totalFuel)
+
+}
+
+func findFuelForMass(mass int) int {
+	fuelNeededForMass := mass/3 - 2
+	if fuelNeededForMass <= 6 {
+		return fuelNeededForMass
+	}
+	return fuelNeededForMass + findFuelForMass(fuelNeededForMass)
 }
