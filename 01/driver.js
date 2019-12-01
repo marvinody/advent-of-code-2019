380f37fee6e69164cf5164c9d3a9e90cc28d5814
Manuel Bugallo <marvinody@gmail.com>
2019-12-01 14:06:32 -0500

    feat: finishd skeleton structure for moving forward each day

---
+++ 01/driver.js
@@ -0,0 +1,25 @@
+const util = require('util');
+const fs = require('fs');
+const path = require('path')
+
+const file = path.join(__dirname, 'input')
+const readFile = util.promisify(fs.readFile);
+
+
+// Change this each time you make a new challenge probably
+
+(async function () {
+  // this is probably always the same
+  const input = await readFile(file, 'utf8')
+
+  // the rest changes
+  const masses = input.split('\n').filter(l => l.length)
+  const { findFuelForMass } = require('./code')
+
+  const totalFuel = masses.reduce((acc, cur) =>
+    acc + findFuelForMass(cur)
+    , 0)
+
+  console.log({ totalFuel })
+
+})()
